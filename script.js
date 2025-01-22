cf37a2ef41a84b9b9787f203f13e03e0af1665ec
leoninadev <149819978+leoninadev@users.noreply.github.com>
2025-01-22 20:11:53 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+
+// Adicionando efeito de rolagem suave aos links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
